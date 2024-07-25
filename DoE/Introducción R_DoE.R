6c2662611cc3aa5efee9d29bf5d18c1fdc4a0bd8
Willandru <willandru@hotmail.com>
2024-07-24 19:26:02 -0500

    Introduction to R

--- DoE/Introducción R_DoE.R
+++ DoE/Introducción R_DoE.R
@@ -15,6 +15,15 @@
 #^ operador binario para potencia.
 #%/% operador binario para obtener el cociente en una divisi?n (n?mero entero).
 #%%  operador binario para obtener el residuo en una divisi?n.
+5+10
+10-2
+3*4
+67/4
+89
+
+60%/%5
+60 %% 5
+
 
 
 
@@ -33,13 +42,26 @@
 #x || y # Disyunci?n vectorial
 #xor(x, y)
 
+5 <= 60
+6 != 9
+
+(6 >60) | (5>8)
 
 # Asignación de variable
+x <- 70
+z <- 5.6
+y <- 'hola'
+y2 <- "hola"
+y == y2
 
+k <- TRUE
 
 # Variables y vectores
 
 
+v1 <- c(6,8,12,1)
+v2 <- c("hola", 34, TRUE, "ADIOS")
+v3 <- c("tx1", "tx2", "tx3")
 # Operaciones basicas con vectores
 #min: para obtener el m?nimo de un vector.
 #max: para obtener el m?ximo de un vector.
@@ -50,25 +72,64 @@
 #which.min: nos entrega la posici?n en donde est? el valor m?nimo del vector.
 #which.max: nos da la posici?n del valor m?ximo del vector.
 #rev: invierte un vector.
-
-
+min(v1)
+max(v1)
+sum(v1)
+prod(v1)
+mean(v1)
+sd(v1)
+var(v1)
+length(v1)
 # Para el seguinete vector calcule la suma entre: la multiplicaci?n de todos los elementos, la longitud del vector y la posici?n
 # en que esta el valor mas peque?o dentro del vector
 
 m <-c(5,9,8,3,2,1)
 
-
+prod(m)
+length(m)
 
 # Manejo de vectores
+m[1]
+m[2]<- 3
+m[1:3]
+m[7] <- 8
+m[9] <- 3
+m
 
+m[c(1,3)]
+m[c(3,1)]
 
 # Funciones
+
+areaRec <- function(largo, ancho){
+  area <- largo*ancho
+  return(area)
+}
+
+z <- areaRec(8,7)
+
 
 # Cree una funci?n que reciba por parametro el peso y la altura de una persona y retorne el IMC. 
 # El IMC se define como el peso divido por el cuadrado de la altura
 
+calcuBMI<-function(peso, altura){
+  BMI<- peso/ (altura^2)
+  return(BMI)
+}
+
+myBMI <- calcuBMI(70, 1.70)
+myBMI
 
 # Condicionales
+
+nota<- 4
+if(nota<3){
+  print("Reprobado")
+}else if(nota >=3 && nota<4){
+  print("Aceptable")
+}else{
+  print("Exelente")
+}
 
 
 # Cree un condicional que imprima "Mayor de edad" o "Menor de edad" segun el valor de la variable e:
@@ -77,8 +138,16 @@ e<-6
 
 # Ciclo
 
+for(i in 1:10){
+  print(i)
+}
+
 
 # Cree un ciclo que imprima en consola la tabla del 5 (del 1 al 10). Use la funci?n print()
+
+for(i in 1:10){
+  print(5*i)
+}
 
 
 # Librerias
